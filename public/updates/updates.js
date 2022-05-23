bd6a6e4c2af4c6ecdd3ef417669af1b8221c3ba9
Vicente Parmigiani <vicenteparmigiani@gmail.com>
2022-05-22 21:39:43 -0300

    Teams fixed

--- public/updates/updates.js
+++ public/updates/updates.js
@@ -1,122 +1,162 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyA07lUMH1HyCjBi_eKe-4gaz1b9FhdSZiE",
-    authDomain: "havarena-f3d87.firebaseapp.com",
-    databaseURL: "https://havarena-f3d87.firebaseio.com",
-    projectId: "havarena-f3d87",
-    storageBucket: "havarena-f3d87.appspot.com",
-    messagingSenderId: "259369291947",
-    appId: "1:259369291947:web:6233862e160cc6bfce67ee",
-    measurementId: "G-STKZ9T8L1C"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+  apiKey: "AIzaSyA07lUMH1HyCjBi_eKe-4gaz1b9FhdSZiE",
+  authDomain: "havarena-f3d87.firebaseapp.com",
+  databaseURL: "https://havarena-f3d87.firebaseio.com",
+  projectId: "havarena-f3d87",
+  storageBucket: "havarena-f3d87.appspot.com",
+  messagingSenderId: "259369291947",
+  appId: "1:259369291947:web:6233862e160cc6bfce67ee",
+  measurementId: "G-STKZ9T8L1C"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
-// Sync points
 
-var fbRef = firebase.database().ref('teams');
+// Buid team cards 
 
-fbRef.on('child_added', function(sn) {
-  document.getElementById('points'+(Number(sn.key)+1)).innerHTML = sn.val().points;
-});
+// Get teams
 
-// Build lists
+firebase.database().ref('teams').once('value', function (snapshot) {
 
-function buildLists() {
-  for (var i = 1; i <= 16; i++) {
-    const listHolder = document.getElementById('activityList'+i);
+  document.getElementById("teamList").innerHTML = "";
 
-    const list = document.createElement('ul');
-    const title = document.createElement('h3');
+  snapshot.forEach(function (childSnapshot) {
+    var childKey = childSnapshot.key;
+    var childData = childSnapshot.val();
 
-    title.innerHTML = 'Atividades concluídas:';
-    title.style.margin = '0px 0px 0px 1vw';
-    listHolder.appendChild(title);
+    // Build team card
+    var team = document.createElement('div');
+    team.className = 'card';
+    team.id = childKey;
 
-    for (var i2 = 0; i2 < activityList.length; i2++) {
-      if (activityList[i2] != null) {
-        const listItem = document.createElement('li');
-        listItem.innerHTML = activityList[i2];
-        listItem.id = 'li/'+i+'/'+i2;
-        listItem.style.display = 'none';
-        list.appendChild(listItem);
-      }
+    // Set card color based on team
+    team.style.backgroundColor = childData.color;
+
+    // Build team name
+    var teamName = document.createElement('span');
+    teamName.innerHTML = childData.name;
+    teamName.className = 'teamName';
+
+    // Build team points
+    var teamPoints = document.createElement('span');
+    teamPoints.className = 'points';
+    teamPoints.innerHTML = childData.points + " pontos";
+
+    // Build team image
+    var teamImage = document.createElement('img');
+    teamImage.className = 'teamImage';
+    teamImage.src = "files/teams/" + (parseInt(team.id) + 1) + ".webp";
+
+    // Create card header and subheader
+    var cardHeader = document.createElement('div');
+    cardHeader.className = 'cardHeader';
+
+    var cardSubheader = document.createElement('div');
+    cardSubheader.className = 'cardSubheader';
+
+    // Append elements to card header and subheader
+    cardSubheader.appendChild(teamName);
+    cardSubheader.appendChild(teamPoints);
+    cardHeader.appendChild(teamImage);
+    cardHeader.appendChild(cardSubheader);
+    team.appendChild(cardHeader);
+
+    // Create card body
+    var cardBody = document.createElement('div');
+    cardBody.className = 'cardBody';
+
+    // List of activities
+    var activities = document.createElement('ul');
+    activities.className = 'activities';
+
+    // List of activities
+    let activityList = [];
+
+    // Get activities from team and put them in an array
+    childSnapshot.child('tasks').forEach(function (childSnapshot) {
+      activityList.push(childSnapshot.key);
+    });
+
+    // If empty, add a message
+    if (activityList.length == 0) {
+      activities.innerHTML += "<li>Nenhuma atividade concluída</li>";
     }
 
-    const noActivityError = document.createElement('p');
-    noActivityError.innerHTML = "Esta equipe ainda não concluiu nenhuma atividade do site.";
-    noActivityError.id = 'noActivityError'+i;
-    noActivityError.style.margin = '1vw';
+    // Build activities list
+    for (let i = 0; i < activityList.length; i++) {
 
-    listHolder.appendChild(list);
-    listHolder.appendChild(noActivityError);
-    listHolder.className = 'listHolder';
-  }
-}
-buildLists();
+      // Get activity data
+      firebase.database().ref('activities/' + activityList[i]).once('value', function (act) {
+        let activity = document.createElement('li');
+        activity.innerHTML = act.val().title;
+        activities.appendChild(activity);
+      });
 
-// Members
+    }
 
-var fbRef1 = firebase.database().ref('users');
+    // Create teams placeholder
+    const teamPlaceholder = document.createElement('div');
+    teamPlaceholder.className = 'teamPlaceholder';
+    teamPlaceholder.id = 'teamPlaceholder+' + childKey;
+    teamPlaceholder.innerHTML = "<h3>Membros da Equipe</h3>";
+    cardBody.appendChild(teamPlaceholder);
 
-fbRef1.on('child_added', function(snapshot) {
-  var displayName = snapshot.val().name;
-  var userTeam = snapshot.val().team;
-  buildMembers(displayName, userTeam);
-});
+    // Append activities to card body
+    cardBody.innerHTML += "<h3>Atividades concluídas</h3>";
+    cardBody.appendChild(activities);
 
-var ggEasy = [];
-function buildMembers(user, team) {
-  const listHolder = document.getElementById('activityList'+team);
+    // Append team to teams
+    team.appendChild(cardBody);
+    document.getElementById('teamList').appendChild(team);
+  })
+}).then(loadTeamMembers);
 
+// Distribute team members to team placeholder
 
-  if (!ggEasy.includes(team)) {
-  const title = document.createElement('h3');
-  title.innerHTML = 'Componentes:';
-  title.style.margin = '20px 0px 0px 1vw';
-  listHolder.appendChild(title);
-  ggEasy.push(team);
-  }
+function loadTeamMembers() {
+  firebase.database().ref("users").once('value', function (snapshot) {
 
-  const componentsHolder = document.createElement('ul');
-  const listItem = document.createElement('li');
-  listItem.innerHTML = user;
-  componentsHolder.appendChild(listItem);
+    snapshot.forEach(function (childSnapshot) {
 
-  listHolder.appendChild(componentsHolder);
-}
+      // Get user data
+      let user = childSnapshot.val();
 
-// Remove pending activities
+      // Get user team
+      let userTeam = user.team;
 
-const fbRef2 = firebase.database().ref('teams');
-fbRef2.on('child_added', function(snapshot) {
-    snapshot.forEach(function(childSnapshot) {
-        if (childSnapshot.key == 'tasks') {
-          childSnapshot.forEach(function(scSnap) {
-            var activity = scSnap.val().done;
-            var aNumber = (scSnap.key-1).toString();
-            var teamNumber = (Number(snapshot.key)+1);
+      // If user has a team
+      if (userTeam != null) {
 
-            if (activity == 'Ok') {
-              document.getElementById('li/'+teamNumber+'/'+aNumber).style.display = 'list-item';
-              document.getElementById('noActivityError'+teamNumber).style.display = 'none';
-            }
-          });
-        };
+        // Get user team placeholder
+        let teamPlaceholder = document.getElementById('teamPlaceholder+' + (userTeam - 1));
+
+        // Create user card
+        let userCard = document.createElement('li');
+        userCard.className = 'userCard';
+
+        // Fill user card with user data
+        userCard.innerHTML = "<text>" + user.name + "</text>";
+
+        // Append user card to team placeholder
+        teamPlaceholder.appendChild(userCard);
+      }
     });
-});
+  });
+}
 
 // Header shadow
 
 headerShadow();
+
 function headerShadow() {
-	if (document.body.scrollTop == 0 || document.documentElement.scrollTop == 0){
-		document.getElementById("header").className = "headerNoShadow";
-	}
+  if (document.body.scrollTop == 0 || document.documentElement.scrollTop == 0) {
+    document.getElementById("header").className = "headerNoShadow";
+  }
   if (document.body.scrollTop != 0 || document.documentElement.scrollTop != 0) {
-		document.getElementById("header").className = "";
-	}
+    document.getElementById("header").className = "";
+  }
 }
 
 // Default code below
@@ -138,7 +178,7 @@ function popup() {
     }
     popupShow = true;
 
-    window.onclick = function() {
+    window.onclick = function () {
       if (event.target != popupMenu && event.target != popupLogged && event.target != userPhoto) {
         popupMenu.className = "popupMenu hide";
         popupLogged.className = "popupMenu hide";
@@ -159,7 +199,7 @@ function loadPage() {
   var user = firebase.auth().currentUser;
   var name, email, photoUrl, uid, emailVerified;
 
-  firebase.auth().onAuthStateChanged(function(user) {
+  firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
       name = user.displayName;
       email = user.email;
@@ -169,21 +209,22 @@ function loadPage() {
 
       document.getElementById('userName').innerHTML = name;
       document.getElementById('userEmail').innerHTML = email;
-      document.getElementById("userPhoto").style.backgroundImage = "url('"+photoUrl+"')";
+      document.getElementById("userPhoto").style.backgroundImage = "url('" + photoUrl + "')";
     }
   });
 }
 
 function signOut() {
-  firebase.auth().signOut().then(function() {
+  firebase.auth().signOut().then(function () {
     console.log('Signed Out');
     location.reload();
-  }, function(error) {
+  }, function (error) {
     console.error('Sign Out Error', error);
   });
 }
 
 var menuOpen = false;
+
 function openMenu() {
   const menu = document.getElementById("menu");
   const menuHolder = document.getElementById("menuHolder");
@@ -192,7 +233,7 @@ function openMenu() {
   if (menuOpen == false) {
     menu.className = "show";
     menuHolder.className = "shadow"
-    window.onclick = function() {
+    window.onclick = function () {
       if (event.target != menu && event.target != sandwich) {
         menu.className = "";
         menuHolder.className = ""
