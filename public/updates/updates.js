a7ee935d9f6315f882e510848cab7566d95e5c5e
Vicente Parmigiani <vicenteparmigiani@gmail.com>
2022-05-27 20:28:56 -0300

    Beautiful colors

--- public/updates/updates.js
+++ public/updates/updates.js
@@ -32,7 +32,8 @@ firebase.database().ref('teams').once('value', function (snapshot) {
     team.id = childKey;
 
     // Set card color based on team
-    team.style.backgroundColor = childData.color;
+    //team.style.backgroundColor = hexToHSL(childData.color);
+    team.style = "background-image: linear-gradient(to bottom right, " + hexToHSL(childData.color, 10) + ", " + hexToHSL(childData.color, -10) + ");";
 
     // Build team name
     var teamName = document.createElement('span');
@@ -261,4 +262,53 @@ function openMenu() {
     menuHolder.className = ""
     menuOpen = false;
   }
+}
+
+// https://css-tricks.com/converting-color-spaces-in-javascript/
+function hexToHSL(H, lvar) {
+
+  // Convert hex to RGB first
+  let r = 0, g = 0, b = 0;
+  if (H.length == 4) {
+    r = "0x" + H[1] + H[1];
+    g = "0x" + H[2] + H[2];
+    b = "0x" + H[3] + H[3];
+  } else if (H.length == 7) {
+    r = "0x" + H[1] + H[2];
+    g = "0x" + H[3] + H[4];
+    b = "0x" + H[5] + H[6];
+  }
+  // Then to HSL
+  r /= 255;
+  g /= 255;
+  b /= 255;
+  let cmin = Math.min(r,g,b),
+      cmax = Math.max(r,g,b),
+      delta = cmax - cmin,
+      h = 0,
+      s = 0,
+      l = 0;
+
+  if (delta == 0)
+    h = 0;
+  else if (cmax == r)
+    h = ((g - b) / delta) % 6;
+  else if (cmax == g)
+    h = (b - r) / delta + 2;
+  else
+    h = (r - g) / delta + 4;
+
+  h = Math.round(h * 60);
+
+  if (h < 0)
+    h += 360;
+
+  l = (cmax + cmin) / 2;
+  s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+  s = +(s * 100).toFixed(1);
+  l = +(l * 100).toFixed(1);
+
+  l += lvar;
+
+  return "hsl(" + h + "," + s + "%," + l + "%)";
 }
